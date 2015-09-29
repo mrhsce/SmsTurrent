461277e681795b9d45406c8b342c8137628b0313
mrhs <mrhsce@gmail.com>
2015-09-29 19:38:57 +0330

    After delete

--- DenseSmsSenderWithGCM/src/main/java/mrhs/ce/DenseSms/MessageLog/DateConvertor.java
+++
@@ -1,6 +0,0 @@
-package mrhs.ce.DenseSms.MessageLog;
-
-public class DateConvertor {
-	// This class should convert the gregorian date to jalali using the algorithm 
-	//used in the real madridiha
-}
